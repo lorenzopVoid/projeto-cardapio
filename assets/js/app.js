6d1c937e2ecd4dbe7e16a40404c4a7b2feb85eef
Lorenzo Eduardo Paulino <lorenzo.paulino1998@gmail.com>
2024-02-27 18:08:35 -0300

    :tada: Adicionado ação do botão ver mais

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,17 +14,28 @@ cardapio.eventos = {
 
 cardapio.methodos = {
     //obtem a lista de itens do cardapio
-    obterItensCardapio: (categoria = 'burgers') => {
+    obterItensCardapio: (categoria = 'burgers', vermais = false) => {
 
         var filtro = MENU[categoria];
         console.log(filtro);
 
-        $("#itensCardapio").html('')
+        if (!vermais) {
+            $("#itensCardapio").html('');
+            $("#btnVerMais").removeClass('hidden');
+        }
 
         $.each(filtro, (i, e) => {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img).replace(/\${name}/g, e.name).replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
 
-            $("#itensCardapio").append(temp)
+            //botão ver mais foi clicado (12 itens)
+            if (vermais && i >= 8 && i < 12) {
+                $("#itensCardapio").append(temp)
+            }
+            // paginaçao inicial (8 itens)
+            if (!vermais && i < 8) {
+                $("#itensCardapio").append(temp)
+            }
+            
         })
 
         // remover o active do elemento
@@ -35,6 +46,15 @@ cardapio.methodos = {
     
 
     },
+    // clique no botão ver mais
+    verMais: () => {
+
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];
+        cardapio.methodos.obterItensCardapio(ativo, true);
+
+        $("#btnVerMais").addClass('hidden');
+
+    }
 }
 
 cardapio.templates = {
